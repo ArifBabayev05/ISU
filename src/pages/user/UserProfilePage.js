d525fd2838ad75d79c5a08674b65f1f2b1f44cfc
Arif Babayev <arifrb@code.edu.az>
2023-06-18 18:56:35 +0400

    User profile page sidebar

--- src/pages/user/UserProfilePage.js
+++ src/pages/user/UserProfilePage.js
@@ -9,61 +9,73 @@ const UserProfilePage = () => {
   };
 
   return (
-    <div className="user-page-container">
-      <div className="sidebar">
-        <ul>
-          <li
-            className={activeTab === 'wants' ? 'active' : ''}
-            onClick={() => handleTabChange('wants')}
-          >
-            Wants
-          </li>
-          <li
-            className={activeTab === 'orders' ? 'active' : ''}
-            onClick={() => handleTabChange('orders')}
-          >
-            Orders
-          </li>
-          <li
-            className={activeTab === 'likeds' ? 'active' : ''}
-            onClick={() => handleTabChange('likeds')}
-          >
-            Likeds
-          </li>
-          <li
-            className={activeTab === 'mydata' ? 'active' : ''}
-            onClick={() => handleTabChange('mydata')}
-          >
-            My Data's
-          </li>
-        </ul>
+    <div className="container user-page-container">
+        <div className="row">
+        <div className='col-lg-3 col-md-12'>
+          <div className="sidebar">
+            <ul>
+              <li className="user-info">
+                Name Surname
+              </li>
+
+              <li
+                className={activeTab === 'wants' ? 'active' : ''}
+                onClick={() => handleTabChange('wants')}
+              >
+                Wants
+              </li>
+              <li
+                className={activeTab === 'orders' ? 'active' : ''}
+                onClick={() => handleTabChange('orders')}
+              >
+                Orders
+              </li>
+              <li
+                className={activeTab === 'likeds' ? 'active' : ''}
+                onClick={() => handleTabChange('likeds')}
+              >
+                Likeds
+              </li>
+              <li
+                className={activeTab === 'mydata' ? 'active' : ''}
+                onClick={() => handleTabChange('mydata')}
+              >
+                My Data's
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className='col-lg-9 col-md-12'>
+          <div className="main-content">
+            {activeTab === 'wants' && (
+              <div>
+                <h2>Wants</h2>
+                {/* Content for the Wants tab */}
+              </div>
+            )}
+            {activeTab === 'orders' && (
+              <div>
+                <h2>Orders</h2>
+                {/* Content for the Orders tab */}
+              </div>
+            )}
+            {activeTab === 'likeds' && (
+              <div>
+                <h2>Likeds</h2>
+                {/* Content for the Likeds tab */}
+              </div>
+            )}
+            {activeTab === 'mydata' && (
+              <div>
+                <h2>My Data's</h2>
+                {/* Content for the My Data's tab */}
+              </div>
+            )}
+          </div>
+        </div>
       </div>
-      <div className="main-content">
-        {activeTab === 'wants' && (
-          <div>
-            <h2>Wants</h2>
-            {/* Content for the Wants tab */}
-          </div>
-        )}
-        {activeTab === 'orders' && (
-          <div>
-            <h2>Orders</h2>
-            {/* Content for the Orders tab */}
-          </div>
-        )}
-        {activeTab === 'likeds' && (
-          <div>
-            <h2>Likeds</h2>
-            {/* Content for the Likeds tab */}
-          </div>
-        )}
-        {activeTab === 'mydata' && (
-          <div>
-            <h2>My Data's</h2>
-            {/* Content for the My Data's tab */}
-          </div>
-        )}
-      </div>
+
+
     </div>
   );
 };
